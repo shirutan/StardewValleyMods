0e6912e9156acec2298f158690c571b9fa376fef
Pet-Slime <52937167+Pet-Slime@users.noreply.github.com>
2024-04-21 02:00:18 -0500

    Updated summery

--- SpaceCore/Api.cs
+++ SpaceCore/Api.cs
@@ -57,6 +57,13 @@ namespace SpaceCore
         /// <param name="farmer"> The farmer you want the list for</param>
         /// <returns>List<Tuple<skill, XP value, Level>></returns>
         List<Tuple<string, int, int>> GetExperienceAndLevelsForCustomSkill(Farmer farmer);
+
+        /// <summary>
+        /// Add EXP to a custom skill
+        /// </summary>
+        /// <param name="farmer"> The farmer who you want to give exp to</param>
+        /// <param name="skill"> The string ID of the custom skill</param>
+        /// <param name="amt"> The int Amount you want to give</param>
         void AddExperienceForCustomSkill(Farmer farmer, string skill, int amt);
 
         /// <summary>
